fde02cf6ff24e2f451b8c898bcbc4f555ab7463a
android2221@gmail.com <android2221@gmail.com>
2018-09-17 22:07:40 -0600

    require click

--- setup.py
+++ setup.py
@@ -12,4 +12,7 @@ setup(
     version='0.1',                          # Update the version number for new releases
     scripts=['get-values'],                 # The name of your scipt, and also the command you'll be using for calling it
     packages=find_packages(),
+    install_requires = [
+        "click==6.7",
+    ]
 )
