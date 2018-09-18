2fcf5d16fabb6d222bf0e97955667dae7aaa5ab2
android2221@gmail.com <android2221@gmail.com>
2018-09-17 21:47:41 -0600

    trying different import

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ except ImportError:
 setup(
     name='helm-get-values',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['get-values']                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['get-values'],                 # The name of your scipt, and also the command you'll be using for calling it
+    packages=find_packages(),
 )
