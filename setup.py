c353cae26aab520f710110858312e47135484a17
android2221@gmail.com <android2221@gmail.com>
2018-09-17 21:18:16 -0600

    pip install stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("setup tools required. Please run: "
+          "pip install setuptools).")
+    exit()
+
+setup(
+    name='helm-get-values',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['get-values']                  # The name of your scipt, and also the command you'll be using for calling it
+)
