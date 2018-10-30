b83c0b53453f58686a7f29c5eb5393ee5a76cdd7
Andy Hablewitz <android2221@gmail.com>
2018-10-29 22:01:30 -0700

    proper command; trying to not install in python scripts folder

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ except ImportError:
 setup(
     name='helm-get-values',    # This is the name of your PyPI-package.
     version='0.1.2',                          # Update the version number for new releases
-    scripts=['get-values'],                 # The name of your scipt, and also the command you'll be using for calling it
     packages=find_packages(),
     install_requires = [
         "click==6.7",
